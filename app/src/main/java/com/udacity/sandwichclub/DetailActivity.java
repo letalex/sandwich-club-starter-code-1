957c286da40797866c27d13c7dbb2a2389cfdd06
letalex <30481650+letalex@users.noreply.github.com>
2018-09-23 18:31:09 -0400

    Updated Code for Nanodegree Sandwich Club

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-
-
+import android.view.View;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -17,24 +18,30 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    ImageView ingredientsIV;
+    TextView also_known_tv_label;
+    TextView also_known_tv;
+    TextView ingredients_tv ;
+    TextView description_tv ;
+    TextView origin_tv ;
+    ImageView ingredientsIv;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ingredientsIv = findViewById(R.id.image_iv);
-        TextView knownAsTv = findViewById(R.id.also_known_tv);
-        TextView originTv = findViewById(R.id.origin_tv);
-        TextView descriptionTv = findViewById(R.id.description_tv);
-        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        ingredientsIV = findViewById(R.id.image_iv);
+
+
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        int position = intent != null ? intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION) : 0;
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -50,10 +57,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(ingredientsIV);
 
         setTitle(sandwich.getMainName());
     }
@@ -65,18 +72,38 @@ public class DetailActivity extends AppCompatActivity {
 
     private void populateUI(Sandwich sandwich) {
         // get alsoKnownAs
-         also_known_tv = (TextView) findViewById( R.id.also_known_tv );
-              if (sandwich.getAlsoKnownAs() !=null && !sandwich.getAlsoKnownAs().isEmpty()){
-                for ( int i =0; i<sandwich.getAlsoKnownAs().size(); i++) {
-                    also_known_tv.append("- " + sandwich.getAlsoKnownAs().get(i) + "\n" );
-                    also_known_tv.append("\n");
-                           
-}
+        also_known_tv_label = findViewById(R.id.also_known_tv_label);
+        also_known_tv = findViewById(R.id.also_known_tv);
+        if ((sandwich.getAlsoKnownAs() != null) && !sandwich.getAlsoKnownAs().isEmpty()) {
+            for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
+                also_known_tv.append("- " + sandwich.getAlsoKnownAs().get(i) + "\n");
 
+            }
+        } else {
+            also_known_tv.setVisibility(View.GONE);
+            also_known_tv_label.setVisibility(View.GONE);
+        }
+        //get ingredients
+        ingredients_tv = findViewById(R.id.ingredients_tv);
+        if (sandwich.getIngredients() != null && !sandwich.getIngredients().isEmpty()) {
+            for (int i = 0; i < sandwich.getIngredients().size(); i++) {
+                ingredients_tv.append("- " + sandwich.getIngredients().get(i) + "\n");
+            }
+        } else {
+            ingredients_tv.setVisibility(View.GONE);
 
-}
+            // get description
+            description_tv = findViewById(R.id.description_tv);
+            description_tv.setText(sandwich.getDescription());
 
-}
+            // get place of origin
+            origin_tv = findViewById(R.id.origin_tv);
 
-    
-
+            if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+                origin_tv.setText(sandwich.getPlaceOfOrigin());
+            } else {
+                origin_tv.setText("");
+            }
+        }
+    }
+    }
